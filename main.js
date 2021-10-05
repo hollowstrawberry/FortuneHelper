d5f2460a88421dcccd8eb6a1b07ff2e53196b0fa
TDarkShadow <5389859+TDarkShadow@users.noreply.github.com>
2021-10-06 00:29:53 +0200

    Add Auto-Harvest Sugar Lump

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ if (!FortuneHelper) var FortuneHelper = {
         fortunesound: 1,
         goldensound: 1,
         muteclick: 0,
-        chocolateegg: 1
+        chocolateegg: 1,
+        sugarlump: 0
     },
 
     isLoaded: false,
@@ -98,6 +99,15 @@ if (!FortuneHelper) var FortuneHelper = {
                     upgrade.buy(1);
                 }
             }
+        }
+
+        // Sugar Lump
+        if (this.config.sugarlump) {
+          if (!Game.canLumps()) return;
+          var age=Date.now()-Game.lumpT;
+			    if (age<Game.lumpMatureAge) return;
+			    else if (age<Game.lumpRipeAge) Game.clickLump();
+			    else if (age<Game.lumpOverripeAge) Game.clickLump();
         }
 
         // Chocolate egg
@@ -196,6 +206,8 @@ if (!FortuneHelper) var FortuneHelper = {
         </div><div class="listing">
             ${this.button('reindeer', 'Click Reindeer ON', 'Click Reindeer OFF')}
             ${this.button('wrinkler', 'Pop Wrinklers ON', 'Pop Wrinklers OFF')}
+        </div><div class="listing">
+            ${this.button('sugarlump', 'Auto-Harvest Sugar Lump ON', 'Auto-Harvest Sugar Lump OFF')}
         </div>
         <br>
         ${this.header('Advanced')}
